b8cab81e3f9a38e473838469064c376c3f55e9f0
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-03-02 11:17:29 +0000

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,7 @@
+echo "# semester2-week6" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/NWDQ0199/semester2-week6.git
+git push -u origin main
